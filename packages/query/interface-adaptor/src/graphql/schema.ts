ba0f98b491fc472b5e8410a04ab0b0abb40a86bb
shibe23 <shibe23.info@gmail.com>
2024-05-26 19:03:15 +0900

    feat: add Query - getAttendanceStamps

--- packages/query/interface-adaptor/src/graphql/schema.ts
+++ packages/query/interface-adaptor/src/graphql/schema.ts
@@ -1,10 +1,10 @@
 import { buildSchema } from "type-graphql";
 import { GraphQLSchema } from "graphql/type";
-import { GroupChatQueryResolver } from "./resolvers";
+import { AttendanceStampQueryResolver, GroupChatQueryResolver } from "./resolvers";
 
 async function createQuerySchema(): Promise<GraphQLSchema> {
   return await buildSchema({
-    resolvers: [GroupChatQueryResolver],
+    resolvers: [GroupChatQueryResolver, AttendanceStampQueryResolver],
     // emitSchemaFile: path.resolve(__dirname, "query.schema.graphql"),
     validate: false,
   });
--- tools/e2e-test/verify-attendance-stamp.sh
+++ tools/e2e-test/verify-attendance-stamp.sh
@@ -3,6 +3,7 @@
 GROUP_CHAT_ID=$(echo $CREATE_GROUP_CHAT_RESULT | jq -r .data.createGroupChat.groupChatId)
 USER_ACCOUNT_ID=${USER_ACCOUNT_ID:-UserAccount-01H7C6DWMK1BKS1JYH1XZE529M}
 WRITE_API_SERVER_BASE_URL=${WRITE_API_SERVER_BASE_URL:-http://localhost:38080}
+READ_API_SERVER_BASE_URL=${READ_API_SERVER_BASE_URL:-http://localhost:38082}
 
 # 勤怠打刻
 echo -e "\nCreate Attendance(${GROUP_CHAT_ID}, ${USER_ACCOUNT_ID}):"
@@ -20,3 +21,21 @@ POST_MESSAGE_RESULT=$(curl -s -X POST -H "Content-Type: application/json" \
 }
 EOS
 )
+
+sleep 1
+
+# 勤怠取得
+echo -e "\nGet AttendanceStamps(${USER_ACCOUNT_ID}, ${ADMIN_ID}):"
+GET_ATTENDANCE_STAMP_RESULT=$(curl -s -X POST -H "Content-Type: application/json" \
+	${READ_API_SERVER_BASE_URL}/query \
+	-d @- <<EOS
+{ "query": "{ getAttendanceStamps(userAccountId: \"${USER_ACCOUNT_ID}\") { id, userAccountId, stampingAt, createdAt, updatedAt } }" }
+EOS
+)
+
+if echo $GET_ATTENDANCE_STAMP_RESULT | jq -e .errors > /dev/null; then
+  echo "Error: $GET_ATTENDANCE_STAMP_RESULT"
+  exit 1
+fi
+
+echo "Result: $GET_ATTENDANCE_STAMP_RESULT"
